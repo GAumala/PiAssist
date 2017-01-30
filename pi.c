1ea25b972c29420ceea5c5a0cf791d7efb39daac
pinkfloyd92 <ascacere92@gmail.com>
2017-01-30 10:37:12 -0500

    added handling of bits using list.

--- pi.c
+++ pi.c
@@ -6,6 +6,38 @@
 
 int pins[10];
 
+
+int *get_bits_from_int(int byte, int bits_wanted){
+  int *bits = malloc(sizeof(int)*bits_wanted); //4 bits or 3 bits...
+
+  for (int i = 0; i < bits_wanted; i++) {
+    int mask = 1<<i; // 00000001 in 1st iteration..
+    int masked_byte = byte & mask;
+    int bit = masked_byte>>i;
+    bits[i] = bit;
+  }
+  return bits;
+}
+
+void write_bytes_to_pi(int pins[], int **values, int length_lista){
+  for (int i; i < length_lista; i++) {
+    //arrows
+    int *arrows = get_bits_from_int(values[i][0], 4);
+    //punchs
+    int *punchs = get_bits_from_int(values[i][1], 3);
+    //kicks
+    int *kicks = get_bits_from_int(values[i][2], 3);
+
+    int *total = malloc(10*sizeof(int));
+    memcpy(total, arrows, 4*sizeof(int));
+    memcpy(total, punchs, 3*sizeof(int));
+    memcpy(total, kicks, 3*sizeof(int));
+
+    write(pins, total);
+    delay(10);
+  }
+}
+
 int initialize_array_from_file(int pins[]){
   FILE *file_pins = fopen("pins_order.txt", "r");
   char* buffer, *token = 0;
@@ -52,9 +84,28 @@ void write(int pins[], int valores[]){
   }
 }
 
+void init_lista_movimientos(int **valores, int dim_x){
+  int dim_y = 3;
+  int i,j,k;
+  int **data;
+  data = (int **) malloc(sizeof(int *) * dim_x);
+  for (k = 0; k < dim_x; k++) {
+    data[k] = (int *) malloc(sizeof(int) * dim_y);
+  }
+
+  for (i = 0; i < dim_x; i++) {
+    for (j = 0; j < dim_y; j++) {
+      data[i][j] = 0; // filling the data with 0s...
+    }
+  }
+  valores = data;
+}
+
 int main(int argc, char *argv[]){
-  int valores[10];
-  int error = initialize_array_from_file(pins);
+  int LENGTH = 10; //length representa la longitud que tiene la lista.
+  int **valores;
+  init_lista_movimientos(valores, LENGTH); // se inicializa la lista.
+  int error = initialize_array_from_file(pins); //se cargan los numeros de los pins de archivo en array pins.
 
   if(error == -0){
     printf("Error loading pins...\n");
@@ -63,10 +114,8 @@ int main(int argc, char *argv[]){
 
   setup(pins);
 
-  //escribimos....
     while(1){
-      write(pins, valores);
-      delay(10);
+      write_bytes_to_pi(pins, valores, LENGTH); //we keep writing bytes to the pins the whole time...
     }
 
   return 0;
--- pi.h
+++ pi.h
@@ -3,3 +3,7 @@
 int initialize_array_from_file(int pins[]);
 void write(int pins[], int valores[]);
 void setup(int pins[]);
+int *get_bits_from_int(int n, int bits_wanted);
+void write_bytes_to_pi(int pins[], int **values, int length_lista);
+void init_lista_movimientos(int **valores, int dim_x); //dim_y es de 3 siempre.
+
