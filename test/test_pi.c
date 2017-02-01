db28d1ee2d4b566e6d8265a07503dc2a95fc26aa
pinkfloyd92 <ascacere92@gmail.com>
2017-01-31 20:00:58 -0500

    added new unit tests.

--- test/test_pi.c
+++ test/test_pi.c
@@ -3,6 +3,29 @@
 #include <stdlib.h>
 
 
+
+
+/*
+  Here we check if our array of pins is properly filled from the file pins_order.txt.
+*/
+START_TEST(initialize_array_from_file_test){
+  uint8_t pins[10];
+  int error;
+  error = initialize_array_from_file(pins);
+  ck_assert_int_eq(error, 1);
+  ck_assert_int_eq(pins[0], 2);
+  ck_assert_int_eq(pins[1], 3);
+  ck_assert_int_eq(pins[2], 4);
+  ck_assert_int_eq(pins[3], 14);
+  ck_assert_int_eq(pins[4], 15);
+  ck_assert_int_eq(pins[5], 18);
+  ck_assert_int_eq(pins[6], 17);
+  ck_assert_int_eq(pins[7], 27);
+  ck_assert_int_eq(pins[8], 22);
+  ck_assert_int_eq(pins[9], 23);
+}
+END_TEST
+
 /*
   Here we check if the queue is being filled with the proper number of elements...
  */
@@ -16,6 +39,66 @@ START_TEST(fill_queue_test){
 }
 END_TEST
 
+/*
+  Here we check ...
+*/
+START_TEST(write_bytes_test){
+  char input_values[11] = "1,2,3,4,5,6";
+  head_t head;
+  TAILQ_INIT(&head);
+  fill_queue(&head, input_values);
+
+  struct node *bytes_to_write;
+  struct node *tmp_bytes_to_write;
+
+  //here we check the first row 123
+  bytes_to_write = TAILQ_FIRST(&head);
+  tmp_bytes_to_write = TAILQ_NEXT(bytes_to_write, nodes);
+  /* write(pins, bytes_to_write->pin_values); //escribimos y luego desencolamos y liberamos memoria. */
+  //arrows
+  uint8_t *arrows = (bytes_to_write->pin_values)[0];
+  //punchs
+  uint8_t *punchs = (bytes_to_write->pin_values)[1];
+  //kicks
+  uint8_t *kicks = (bytes_to_write->pin_values[2]);
+
+  uint8_t *total = malloc(10*sizeof(uint8_t));
+
+  ck_assert_int_eq(arrows[0], 1);
+  ck_assert_int_eq(arrows[1], 0);
+  ck_assert_int_eq(arrows[2], 0);
+  ck_assert_int_eq(arrows[3], 0);
+  ck_assert_int_eq(punchs[0], 0);
+  ck_assert_int_eq(punchs[1], 1);
+  ck_assert_int_eq(punchs[2], 0);
+  ck_assert_int_eq(kicks[0], 1);
+  ck_assert_int_eq(kicks[1], 1);
+  ck_assert_int_eq(kicks[2], 0);
+  // here we check the 2nd row.
+  bytes_to_write = tmp_bytes_to_write;
+
+  arrows = (bytes_to_write->pin_values)[0];
+  //punchs
+  punchs = (bytes_to_write->pin_values)[1];
+  //kicks
+  kicks = (bytes_to_write->pin_values[2]);
+  ck_assert_int_eq(arrows[0], 0);
+  ck_assert_int_eq(arrows[1], 0);
+  ck_assert_int_eq(arrows[2], 1);
+  ck_assert_int_eq(arrows[3], 0);
+  ck_assert_int_eq(punchs[0], 1);
+  ck_assert_int_eq(punchs[1], 0);
+  ck_assert_int_eq(punchs[2], 1);
+  ck_assert_int_eq(kicks[0], 0);
+  ck_assert_int_eq(kicks[1], 1);
+  ck_assert_int_eq(kicks[2], 1);
+  free(bytes_to_write);
+
+}
+END_TEST
+
+
+
 Suite * queue_suite(void)
 {
   Suite *s;
@@ -26,6 +109,8 @@ Suite * queue_suite(void)
   tc_core = tcase_create("Core");
 
   tcase_add_test(tc_core, fill_queue_test);
+  tcase_add_test(tc_core, initialize_array_from_file_test);
+  tcase_add_test(tc_core, write_bytes_test);
   suite_add_tcase(s, tc_core);
 
   return s;
