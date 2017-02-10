2bdf4bb9537d018e7858d6980a8fefb46b1606d2
Gabriel Aumala <gabrielfima@live.com>
2017-02-10 11:44:17 -0500

    Get data to publish from STDIN

--- Python-Mosquitto/publisher.py
+++ Python-Mosquitto/publisher.py
@@ -2,26 +2,19 @@ import sys
 import paho.mqtt.client as mqtt
 import os
 import socket
+import fileinput
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("google.com", 80))
-HOST = s.getsockname()[0]
-ARCHIVO = "content_to_send.txt"
-s.close()
+TOPIC = "tas"
+HOST = "192.168.0.7"
 
-try:
-    TOPIC = str(sys.argv[1])
+# get data from STDIN
+content = ""
+for line in fileinput.input():
+    content += line
 
-except:
-    print("Missing topic.")
-    sys.exit()
-
+# publish
 client = mqtt.Client()
-os.chdir("../pi")  # changing our current working directory.
 client.connect(HOST, 1883, 60)
-archivo = open(ARCHIVO, 'r')
-content = archivo.read()
-archivo.close()
 client.publish(TOPIC, content, 0, False)
 
-print("Published new content in channel " + TOPIC)
+print("Published new content in channel " + TOPIC + " from host " + HOST)
